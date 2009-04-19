b9039a36b06dba9dc30a9f551391c6b91a1408ae
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2009-04-19 11:39:48 +0000

    * merge ourea

---
+++ ourea/source/sidux-ourea.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+./apt2svg *_Packages.gz ../wallpaper/sidux-ourea.svg ../wallpaper/sidux-ourea-wide.svg
+#  convert ../wallpaper/sidux-ourea-wide.svg ../wallpaper/sidux-ourea-wide.jpg
+#  convert ../wallpaper/sidux-ourea.svg ../wallpaper/sidux-ourea.jpg
+# ./apt2svg --nobg *_Packages.gz ../wallpaper/sidux-ourea-nobg.svg ../wallpaper/sidux-ourea-wide-nobg.svg
+# convert ../wallpaper/sidux-ourea-wide-nobg.svg ../wallpaper/sidux-ourea-wide-nobg.png
+# convert ../wallpaper/sidux-ourea-nobg.svg ../wallpaper/sidux-ourea-nobg.png
